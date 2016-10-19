e680426e6c3d6f329343eef1504d414ed3ab6b9d
pablosanjuan <pablosanjuanm@gmail.com>
2016-10-19 15:09:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/seratic/drawerfalabella/Splash.java
@@ -0,0 +1,37 @@
+package com.example.seratic.drawerfalabella;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask task = new TimerTask() {
+            public void run() {
+                ingresar();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+
+    private void ingresar() {
+        Intent i = new Intent().setClass(Splash.this, MainActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
+        finish();
+    }
+}
